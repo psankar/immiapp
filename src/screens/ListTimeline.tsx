9ebfb4936ce621f3e0aa0e9d990fb7c3c2fa07fa
Sankar சங்கர் <sankar.curiosity@gmail.com>
2023-10-31 23:03:33 +0530

    Listen to streams

--- src/screens/ListTimeline.tsx
+++ src/screens/ListTimeline.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import { saxios } from "../context/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext, AuthContextType, saxios } from "../context/AuthContext";
+import BASE_URL from "../config";
 
 // TODO: Use more precise types below
 type Props = {
@@ -9,28 +10,43 @@ type Props = {
 
 const ListTimeline = ({ route }: Props) => {
   const { handle, displayName } = route.params;
+  var [immiIDs, setImmiIDs] = useState<string[]>([]);
+
+  const { authToken } = useContext<AuthContextType>(AuthContext);
+
   useEffect(() => {
     const fetchData = async () => {
-      console.log("listTimeline", displayName, handle);
-      saxios
-        .post(
-          "/read-list",
-          {
-            list_handle: handle,
+      try {
+        const response = await fetch(`${BASE_URL}/read-list`, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            "Content-Type": "text/event-stream",
           },
-          {
-            onDownloadProgress: (progressEvent) => {
-              const newChunk = progressEvent.event.target.response;
-              if (newChunk) {
-                console.log(newChunk);
-                console.log(typeof newChunk);
-              }
-            },
-          }
-        )
-        .catch((error) => {
-          console.log(error);
+          body: JSON.stringify({ list_handle: handle }),
         });
+
+        if (!response.body) {
+          throw new Error("Response body is null");
+        }
+
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder("utf-8");
+
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) break;
+          const text = decoder.decode(value, { stream: true });
+          const newData = JSON.parse(text);
+          if (newData && newData.length > 0) {
+            const l = [...newData, ...immiIDs];
+            immiIDs = l;
+          }
+          console.log(immiIDs, newData);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
     fetchData();
   }, []);
