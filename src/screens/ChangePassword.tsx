cdab291332908c4bd64bdc761f309e345ba89a4c
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-03-03 15:55:25 +0530

    navigation controls for changepassword screen

--- src/screens/ChangePassword.tsx
+++ src/screens/ChangePassword.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   ActivityIndicator,
+  Modal,
   Pressable,
   StyleSheet,
   Text,
@@ -10,31 +11,42 @@ import {
 import { isValidPassword } from "../constants/global-constants";
 import { saxios } from "../context/AuthContext";
 import t from "../localization/i18n";
+import { NavigationProp } from "@react-navigation/native";
 
-export const ChangePassword = () => {
+type ChangePasswordProps = {
+  navigation: NavigationProp<Record<string, object>>;
+};
+
+export const ChangePassword = ({ navigation }: ChangePasswordProps) => {
   const [existingPassword, setExistingPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [isValid, setIsValid] = useState(false);
+  const [passwordChanged, setPasswordChanged] = useState(false);
 
   const onExistingPasswordInputChange = (text: string) => {
     setExistingPassword(text);
-    validateInputs();
+    validateInputs(text, newPassword, repeatPassword);
   };
 
   const onNewPasswordInputChange = (text: string) => {
     setNewPassword(text);
-    validateInputs();
+    validateInputs(existingPassword, text, repeatPassword);
   };
 
   const onRepeatPasswordInputChange = (text: string) => {
     setRepeatPassword(text);
-    validateInputs();
+    validateInputs(existingPassword, newPassword, text);
   };
 
-  const validateInputs = () => {
+  const validateInputs = (
+    existingPassword: string,
+    newPassword: string,
+    repeatPassword: string
+  ) => {
+    console.debug();
     setIsValid(
       isValidPassword(existingPassword) &&
         isValidPassword(newPassword) &&
@@ -58,18 +70,27 @@ export const ChangePassword = () => {
     }
 
     setIsLoading(true);
-    try {
-      await saxios.post("/change-password", {
+    saxios
+      .post("/change-password", {
         existing_password: existingPassword,
         new_password: newPassword,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Password change succeeded", response);
+          setPasswordChanged(true);
+        }
+      })
+      .catch((error) => {
+        console.error("Password change failed", error);
+        setError(t("password_change_failed"));
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setExistingPassword("");
+        setNewPassword("");
+        setRepeatPassword("");
       });
-      setError("");
-    } catch (err) {
-      setError(t("password_change_failed"));
-      console.error(err);
-    } finally {
-      setIsLoading(false);
-    }
   };
 
   if (isLoading) {
@@ -80,6 +101,25 @@ export const ChangePassword = () => {
     );
   }
 
+  if (passwordChanged) {
+    return (
+      <Modal visible={true} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>{t("password_changed")}</Text>
+          <Pressable
+            onPress={() => {
+              setPasswordChanged(false);
+              navigation.goBack();
+            }}
+            style={styles.modalButton}
+          >
+            <Text style={styles.modalButtonText}>OK</Text>
+          </Pressable>
+        </View>
+      </Modal>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{t("password_existing")}</Text>
@@ -87,7 +127,7 @@ export const ChangePassword = () => {
         style={styles.input}
         secureTextEntry
         placeholder={t("password")}
-        value={newPassword}
+        value={existingPassword}
         onChangeText={onExistingPasswordInputChange}
       />
       <Text style={styles.label}>{t("password_new")}</Text>
@@ -113,7 +153,7 @@ export const ChangePassword = () => {
       >
         <Text style={styles.buttonText}>{t("change_password")}</Text>
       </Pressable>
-      {error && <Text style={styles.error}>{error}</Text>}
+      {error ? <Text style={styles.error}>{error}</Text> : null}
     </View>
   );
 };
@@ -167,5 +207,28 @@ const styles = StyleSheet.create({
   error: {
     color: "red",
     marginTop: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  modalButton: {
+    backgroundColor: "#f00",
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: "center",
+  },
+  modalButtonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
