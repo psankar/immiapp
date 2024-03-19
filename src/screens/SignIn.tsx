74400ea62933532529b74a0d5d70f2db9cfc2487
Sankar சங்கர் <sankar.curiosity@gmail.com>
2024-03-19 19:19:40 +0530

    experiment with tailwind

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,9 @@
 import { Ionicons } from "@expo/vector-icons";
-import { NavigationProp, useIsFocused } from "@react-navigation/native";
+import {
+  NavigationProp,
+  ThemeProvider,
+  useIsFocused,
+} from "@react-navigation/native";
 import axios from "axios";
 import { styled } from "nativewind";
 import React, { useContext, useEffect, useState } from "react";
@@ -151,7 +155,87 @@ export const SignIn = ({ navigation }: SignInProps) => {
   }
 
   return (
-    <StyledView className="flex-1 justify-center items-center bg-gray-100">
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+/*
+
+<StyledView className="flex-1">
       <StyledView className="w-4/5 bg-white rounded-lg shadow-lg p-8">
         <StyledText className="text-lg font-bold mb-4">
           {t("account_handle")}
@@ -205,5 +289,5 @@ export const SignIn = ({ navigation }: SignInProps) => {
         ) : null}
       </StyledView>
     </StyledView>
-  );
-};
+
+*/
